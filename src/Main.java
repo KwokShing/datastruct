f2f3b533689c1d447a80b3695902240881ca9d44
KwokShing <kwokshingchan92@gmail.com>
2017-07-26 10:15:12 -0700

    ttt

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@
 public class Main {
     public static void main(String[] args) {
         RedBlackTree<Integer> tree = new RedBlackTree<>();
-        System.out.println();
+        System.out.println("ttt");
     }
 }
